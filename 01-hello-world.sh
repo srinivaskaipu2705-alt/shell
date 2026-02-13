2528a6faa438fb26895cd6625117b65640c0ba57
srini <srinivaskaipu27@gmail.com>
2026-02-12 23:32:17 -0500

    conversation

---
+++ 01-hello-world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#she bang line, tells the system to use bash to execute this script
+
+echo "Hello, World!"
+#echo is a command that prints the text to the terminal
---
+++ 02-conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#she bang line, tells the system to use bash to execute this script
+
+echo "sri:: hi sai, how are you doing?"
+echo "sai:: hi sri, i am doing good, how about you?"
+echo "sri:: i am doing good too, thanks for asking!"
+echo "sri:: do you want to grab some coffee later?"
+echo "sai:: sure, that sounds great! what time do you want to meet?"
+echo "sri:: how about 3pm?"
+echo "sai:: that works for me, see you then!"

30c9442344adaa3692ad52aca55588037a69d12b
srini <srinivaskaipu27@gmail.com>
2026-02-19 16:26:56 -0500

    variables

--- 15-colors.sh
+++ 15-colors.sh
@@ -6,6 +6,6 @@ Y='\033[0;33m'
 B='\033[0;34m'
 N='\033[0m' # No Color
 
-echo -e "${R}This is red text${N}"
-echo -e "${G}This is green text${N}"
-echo -e "${Y}This is yellow text${N}"         
+echo -e "$R This is red text $N"
+echo -e "$G  This is green text $N"
+echo -e "$Y This is yellow text $N"         

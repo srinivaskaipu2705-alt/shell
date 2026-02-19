ec1412fd2c9b873cebd304d86f720938ae4cd565
srini <srinivaskaipu27@gmail.com>
2026-02-19 14:50:26 -0500

    variables

--- 11-if-else-conditions.sh
+++ 11-if-else-conditions.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 NUMBER=$1
+NUMBER2=$2
 
 if [ $NUMBER -lt 10 ]; then
     echo "the number is less than 10"
     else
     echo "the number is greater than or equal to 10"
 fi
+
+
+#########
+
 
 if [ $NUMBER -lt 10 ]; then
     echo "the number is less than 10"

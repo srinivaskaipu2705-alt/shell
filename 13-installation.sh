1f6df91fa06c945d47e36e778ce1f44b4539a765
srini <srinivaskaipu27@gmail.com>
2026-02-19 15:39:10 -0500

    variables

---
+++ 13-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -eq 0 ]; then
+    echo "i am root user"
+    exit 0
+    else 
+    echo "erroe:: you are not root user, please run this script as root user"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]; then
+    echo "mysql installation successful"
+    else 
+    echo "error:: mysql installation failed"
+    exit 1
+fi

ff4ab8b1efe8249b41527fd95fed2f55ce6fc83d
srini <srinivaskaipu27@gmail.com>
2026-02-19 14:44:44 -0500

    variables

--- 11-if-else-conditions.sh
+++ 11-if-else-conditions.sh
@@ -7,3 +7,11 @@ if [ $NUMBER -lt 10 ]; then
     else
     echo "the number is greater than or equal to 10"
 fi
+
+if [ $NUMBER -lt 10 ]; then
+    echo "the number is less than 10"
+elif [ $NUMBER -eq 10 ]; then
+    echo "the number is equal to 10"
+else
+    echo "the number is greater than 10"
+fi
